fac695d0a018920b604c51eec255efd012bc5bfa
Mario <982254249@qq.com>
2021-04-25 23:00:14 +0800

    version 1: only front-end finished

--- ui/vue.config.js
+++ ui/vue.config.js
@@ -4,6 +4,18 @@ module.exports = {
   publicPath: "./",
   assetsDir: "assets",
   productionSourceMap: false,
+  pages: {
+    index: {
+      entry: "src/main.ts",
+      template: "public/index.html",
+      filename: "index.html"
+    },
+    unsupported: {
+      entry: "src/views/unsupported/main.ts",
+      template: "src/views/unsupported/unsupported.html",
+      filename: "unsupported.html"
+    }
+  },
   devServer: {
     open: true,
     port: 9693
